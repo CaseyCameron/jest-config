0b0b5b16c8eb13bc63aa89449fb90a22f663f9be
caseycameron <mcasey.cameron@gmail.com>
2022-07-18 10:39:04 -0700

    finished calculator

--- src/shared/__tests__/calculator.js
+++ src/shared/__tests__/calculator.js
@@ -3,5 +3,12 @@ import {render, fireEvent, screen} from '@testing-library/react'
 import Calculator from '../../calculator'
 
 test('the clear button switches from AC to C when there is an entry', () => {
+  render(<Calculator />)
+  const clearButton = screen.getByText('AC')
 
+  fireEvent.click(screen.getByText(/3/))
+  expect(clearButton).toHaveTextContent('C')
+
+  fireEvent.click(clearButton)
+  expect(clearButton).toHaveTextContent('AC')
 })
