f6055531a377fd63318ab02129aeed5e885a4e60
caseycameron <mcasey.cameron@gmail.com>
2022-07-18 10:36:16 -0700

    setting up calculator test

---
+++ src/shared/__tests__/calculator.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {render, fireEvent, screen} from '@testing-library/react'
+import Calculator from '../../calculator'
+
+test('the clear button switches from AC to C when there is an entry', () => {
+
+})
